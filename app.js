f02a7ef8cd5043a9269c6bf29a56498ebabb09d3
Danil <danil.kersontsev@khk.ee>
2022-09-20 11:29:13 +0300

    Symbol,null ja string

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 console.log("Hello world! ")
 // variables
-// let
+// string
 let name = 'Danil Kersontsev'
-name = "New name"
+console.log(typeof name)
+let goodmark = 5
+console.log(goodmark)
+console.log(typeof goodmark)
+let ishere = true
+console.log(ishere)
+console.log(typeof ishere)
 //const
 const surname = 'Kersontsev'
 // surname = "Kersontsev" - error
@@ -17,6 +23,19 @@ const person = {
     firstname:'Danil',
     surname: 'Kersontsev'
 }
-person.department = 'IKT'
+
 console.log(person)
+console.log(typeof person)
+// null
+const airplane = null
+console.log(airplane)
+console.log(typeof airplane)
+//symbol
+const symbol = Symbol()
+console.log(symbol)
+console.log(typeof symbol)
+
+let test
+console.log(test)
+console.log(typeof test)
 
