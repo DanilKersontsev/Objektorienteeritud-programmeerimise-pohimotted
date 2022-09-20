878357eed791539272a43511c2e49572ec00fe32
Danil <danil.kersontsev@khk.ee>
2022-09-20 11:17:42 +0300

    Variables

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 console.log("Hello world! ")
+// variables
+// let
+let name = 'Danil Kersontsev'
+name = "New name"
+//const
+const surname = 'Kersontsev'
+// surname = "Kersontsev" - error
+
+const numbers = [1, 2, 3, 4]
+console.log(numbers)
+numbers.push(6)
+console.log(numbers)
+
+//object
+const person = {
+    firstname:'Danil',
+    surname: 'Kersontsev'
+}
+person.department = 'IKT'
+console.log(person)
+
